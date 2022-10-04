eb1b21fb2591e858e434b0c74ed1b6e02f1415cf
Jianh <jianh2@Nycstudents.ent>
2022-10-03 23:21:40 -0400

    Lab2

--- src/ChatbotRunner.java
+++ src/ChatbotRunner.java
@@ -1,5 +1,23 @@
-public class ChatbotRunner {
-    ChatBot newChat = new ChatBot( "Bob", 6);
-    newChat.weather();
+
+ class ChatBotRunner {
+    public static void main(String[] args) {
+
+
+
+
+        ChatBot newChat = new ChatBot("Debbie", 6);
+        newChat.greeting("hi");
+        newChat.favoriteNumber(4);
+        newChat.weather();
+        newChat.convertFeetToMeters(5);
+        newChat.goodbye();
+        newChat.addNumbers(3,4,5);
+        newChat.favoriteFood("pizza");
+        newChat.Multiply(2,6);
+
+
+
+
+    }
 
 }
